11e005b5a2d78bafa916f072d00510f6345ea50f
danylo-macelai <danylomacelai@gmail.com>
2018-11-16 22:27:07 -0200

    Entidades do projeto virtual-master

---
+++ virtual-master/src/main/java/br/com/master/domain/ArquivoTO.java
@@ -0,0 +1,72 @@
+package br.com.master.domain;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+
+import br.com.common.domain.Domain;
+
+/**
+ * <b>Description:</b> <br>
+ * <b>Project:</b> virtual-master <br>
+ *
+ * @author macelai
+ * @date: 16 de nov de 2018
+ */
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "ARQUIVO")
+public class ArquivoTO extends Domain {
+
+    @Column(name = "tamanho", updatable = false, nullable = false)
+    private Long         tamanho;
+
+    @Column(name = "nome", nullable = false)
+    private String       nome;
+
+    @Column(name = "mime_type", updatable = false, nullable = false)
+    private String       mimeType;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "arquivo")
+    @OrderBy("numero ASC")
+    private Set<BlocoTO> blocos;
+
+    public Long getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(Long tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public Set<BlocoTO> getBlocos() {
+        return blocos;
+    }
+
+    public void setBlocos(Set<BlocoTO> blocos) {
+        this.blocos = blocos;
+    }
+
+}
---
+++ virtual-master/src/main/java/br/com/master/domain/BlocoTO.java
@@ -0,0 +1,68 @@
+package br.com.master.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import br.com.common.domain.Domain;
+
+/**
+ * <b>Description:</b> <br>
+ * <b>Project:</b> virtual-master <br>
+ *
+ * @author macelai
+ * @date: 16 de nov de 2018
+ */
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "BLOCO")
+public class BlocoTO extends Domain {
+
+    @Column(name = "uuid", updatable = false, nullable = false)
+    private String    uuid;
+
+    @Column(name = "numero", updatable = false, nullable = false)
+    private Integer   numero;
+
+    @Column(name = "host", updatable = false, nullable = false)
+    private String    host;
+
+    @ManyToOne
+    @JoinColumn(name = "id_arquivo", updatable = false, nullable = false)
+    private ArquivoTO arquivo;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public ArquivoTO getArquivo() {
+        return arquivo;
+    }
+
+    public void setArquivo(ArquivoTO arquivo) {
+        this.arquivo = arquivo;
+    }
+
+}

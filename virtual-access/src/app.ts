877fba5c774ea19cd5482b632ac3fb18fb8ee502
danylo-macelai <danylomacelai@gmail.com>
2019-05-13 10:26:48 -0300

    Instalação/configuração do express.

--- virtual-access/src/app.ts
+++ virtual-access/src/app.ts
@@ -1,1 +1,25 @@
-console.log('Repositório Virtual!');
+import * as express from 'express';
+
+class App {
+
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+        this.middleware();
+    }
+
+    private middleware(): void {
+
+        this.express.use("/usuarios", (req, res, nex) => {
+            res.send(
+                {
+                    nome: "Danylo Macelai"
+                }
+            );
+        });
+
+    }
+}
+
+export default new App().express;

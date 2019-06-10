c9f3bd6ae57448fdb11d16eafe42dd278a55e410
Renato Araujo <orenatoaraujo@hotmail.com>
2019-06-10 20:09:47 -0300

    Code style do VS Code

--- virtual-access/src/app.ts
+++ virtual-access/src/app.ts
@@ -1,23 +1,25 @@
-import * as express from 'express';
-import * as graphqlHTTP from 'express-graphql';
+import * as express from "express";
+import * as graphqlHTTP from "express-graphql";
 
-import schema from './graphql/schema';
+import schema from "./graphql/schema";
 
 class App {
+  public express: express.Application;
 
-    public express: express.Application;
+  constructor() {
+    this.express = express();
+    this.middleware();
+  }
 
-    constructor() {
-        this.express = express();
-        this.middleware();
-    }
-
-    private middleware(): void {
-        this.express.use("/usuarios", graphqlHTTP({
-            schema: schema,
-            graphiql: process.env.NODE_ENV === 'development'
-        }));
-    }
+  private middleware(): void {
+    this.express.use(
+      "/usuarios",
+      graphqlHTTP({
+        schema: schema,
+        graphiql: process.env.NODE_ENV === "development"
+      })
+    );
+  }
 }
 
 export default new App().express;
